32af4db3dfa20c79a66d148edaf44fa29c1ecdb6
Bertan Orgun <bertanorgun@gmail.com>
2024-07-02 10:54:23 +0300

    selecting element updated
    dice display will be config

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+// Score texts
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+//Score Displays
